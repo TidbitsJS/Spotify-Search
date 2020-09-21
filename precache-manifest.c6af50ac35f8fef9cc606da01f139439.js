8524f3d25e559d587fa5489aac4cfd16b0572c2c
sujata <gunalesujata@gmail.com>
2020-09-21 20:21:54 +0530

    Updates

---
+++ precache-manifest.c6af50ac35f8fef9cc606da01f139439.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50572f933abc75e781fc14fc3a1b9430",
+    "url": "/Spotify-Search/index.html"
+  },
+  {
+    "revision": "c2439e9e460130041462",
+    "url": "/Spotify-Search/static/css/main.4d5a4b0d.chunk.css"
+  },
+  {
+    "revision": "36d21cbf125ec5c11a52",
+    "url": "/Spotify-Search/static/js/2.3e27b155.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spotify-Search/static/js/2.3e27b155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2439e9e460130041462",
+    "url": "/Spotify-Search/static/js/main.93960da4.chunk.js"
+  },
+  {
+    "revision": "c935e666ccb1e189152b",
+    "url": "/Spotify-Search/static/js/runtime-main.300da836.js"
+  },
+  {
+    "revision": "dbc1313b9451dfb681de62a6fb8d6048",
+    "url": "/Spotify-Search/static/media/blues.dbc1313b.jpeg"
+  },
+  {
+    "revision": "8dfabb9f6b5e33dd318e2d42911e032e",
+    "url": "/Spotify-Search/static/media/charts.8dfabb9f.jpeg"
+  },
+  {
+    "revision": "2e609e4fb48cd26677f2d06a122c7a8c",
+    "url": "/Spotify-Search/static/media/chill.2e609e4f.jpeg"
+  },
+  {
+    "revision": "7f5b2564409ac28807e1bc507b8879fb",
+    "url": "/Spotify-Search/static/media/comedy.7f5b2564.jpeg"
+  },
+  {
+    "revision": "5f0dd902dc25e03ef8824497f0ca65f0",
+    "url": "/Spotify-Search/static/media/focus.5f0dd902.jpeg"
+  },
+  {
+    "revision": "ebc6fe0037e664b9c75eb719b361967d",
+    "url": "/Spotify-Search/static/media/gaming.ebc6fe00.png"
+  },
+  {
+    "revision": "50a0687aa1d1345847636d9a336aec9d",
+    "url": "/Spotify-Search/static/media/hiphop.50a0687a.jpeg"
+  },
+  {
+    "revision": "bfbf01aed50b21cef38589607e3f33cf",
+    "url": "/Spotify-Search/static/media/home.bfbf01ae.png"
+  },
+  {
+    "revision": "dce83e88dcc7f8379b1e83c86d7fd771",
+    "url": "/Spotify-Search/static/media/jazz.dce83e88.jpeg"
+  },
+  {
+    "revision": "5c569c94245ec5f6f6424c7f7d16abfd",
+    "url": "/Spotify-Search/static/media/love.5c569c94.jpeg"
+  },
+  {
+    "revision": "9ecfac6a798e382188b2a17bafc357a5",
+    "url": "/Spotify-Search/static/media/mush.9ecfac6a.jpeg"
+  },
+  {
+    "revision": "b0c104cb3d994c47a9a12d788df1d4d1",
+    "url": "/Spotify-Search/static/media/party.b0c104cb.jpeg"
+  },
+  {
+    "revision": "bd38c567c19d942d854ef60c8fe3a615",
+    "url": "/Spotify-Search/static/media/punjabi.bd38c567.jpeg"
+  },
+  {
+    "revision": "5dc4ee8e9826b628ee782c97d52f48c8",
+    "url": "/Spotify-Search/static/media/telgu.5dc4ee8e.jpeg"
+  },
+  {
+    "revision": "11c521997db2bf0aff2af731bda2e0e6",
+    "url": "/Spotify-Search/static/media/you.11c52199.jpeg"
+  }
+]);
